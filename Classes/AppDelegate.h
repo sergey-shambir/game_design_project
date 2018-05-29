cf60dac582d495215354d229c5ac33ab11146c9f
Sergey Shambir <sshambir@yandex.ru>
2018-05-29 12:46:35 +0300

    Настроил размеры окна, переформатировал код

--- Classes/AppDelegate.h
+++ Classes/AppDelegate.h
@@ -22,8 +22,8 @@
  THE SOFTWARE.
  ****************************************************************************/
 
-#ifndef  _APP_DELEGATE_H_
-#define  _APP_DELEGATE_H_
+#ifndef _APP_DELEGATE_H_
+#define _APP_DELEGATE_H_
 
 #include "cocos2d.h"
 
@@ -32,33 +32,32 @@
 
 Private inheritance here hides part of interface from Director.
 */
-class  AppDelegate : private cocos2d::Application
+class AppDelegate : private cocos2d::Application
 {
 public:
-    AppDelegate();
-    virtual ~AppDelegate();
+	AppDelegate();
+	virtual ~AppDelegate();
 
-    virtual void initGLContextAttrs();
+	virtual void initGLContextAttrs();
 
-    /**
+	/**
     @brief    Implement Director and Scene init code here.
     @return true    Initialize success, app continue.
     @return false   Initialize failed, app terminate.
     */
-    virtual bool applicationDidFinishLaunching();
+	virtual bool applicationDidFinishLaunching();
 
-    /**
+	/**
     @brief  Called when the application moves to the background
     @param  the pointer of the application
     */
-    virtual void applicationDidEnterBackground();
+	virtual void applicationDidEnterBackground();
 
-    /**
+	/**
     @brief  Called when the application reenters the foreground
     @param  the pointer of the application
     */
-    virtual void applicationWillEnterForeground();
+	virtual void applicationWillEnterForeground();
 };
 
 #endif // _APP_DELEGATE_H_
-
